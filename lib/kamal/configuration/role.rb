18fc247ad2c48750eb984ca6b2dd6b268a07e935
Fran Zekan <zekan.fran369@gmail.com>
2023-09-04 19:17:10 +0200

    [ENV overloading + optional env syntax (that we don't use any more)]

--- lib/kamal/configuration/role.rb
+++ lib/kamal/configuration/role.rb
@@ -146,8 +146,22 @@ class Kamal::Configuration::Role
         clear_role_env = specialized_env["secret"] ? Array(specialized_env["clear"]) : Array(specialized_env["clear"] || specialized_env)
 
         new_env["clear"] = (clear_app_env + clear_role_env).uniq
+
+        secrets_app_env = Array(config.env["secret"])
+        secrets_role_env = Array(specialized_env["secret"])
+        new_env["secret"] = (secrets_app_env + secrets_role_env).uniq.filter { |secret| filter_secret_env(secret, new_env) }
       end
     end
+
+  def filter_secret_env(secret, new_env)
+    # allow clear to override secret
+    return false if new_env['clear'].include?(secret)
+
+    # if we find FOO but FOO? exists, we keep the FOO?
+    return false if !secret.end_with?('?') && new_env['secret'].include?("#{secret}?")
+
+    true
+  end
 
     def http_health_check(port:, path:)
       "curl -f #{URI.join("http://localhost:#{port}", path)} || exit 1" if path.present? || port.present?
